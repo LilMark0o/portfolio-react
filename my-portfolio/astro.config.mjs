efeb0706927e7c69573cdc725fbc9a7a9b885458
LilMark0o <marcoalejandroramirezc@gmail.com>
2025-03-22 05:00:21 -0500

    all working nicely

--- my-portfolio/astro.config.mjs
+++ my-portfolio/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static', // Ensures Astro generates static files
-  base: '/portfolio/', // Your GitHub repository name
+  base: '/portfolio-react/', // Your GitHub repository name
   site: 'https://lilmark0o.github.io', // Your GitHub Pages site
 });
