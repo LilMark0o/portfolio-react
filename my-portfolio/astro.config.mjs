2d3a00faa5020241e935466f7712fd322b2401f4
LilMark0o <marcoalejandroramirezc@gmail.com>
2025-03-27 23:51:05 -0500

    idk santa cachucha

--- my-portfolio/astro.config.mjs
+++ my-portfolio/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/portfolio-react/', // Your GitHub repository name
+  base: '/', // Your GitHub repository name
   site: 'https://lilmark0o.github.io', // Your GitHub Pages site
 });
